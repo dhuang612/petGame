4f038473fbe4738560ac66c50915487f25df3fdc
dhuang612 <dhuang@madisoncollege.edu>
2019-07-24 17:21:57 -0400

    first commit

---
+++ pet-game/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import feline from './assets/images/cat.jpeg';
+import dino from './assets/images/dinosaur.jpeg';
+import canine from './assets/images/dog.jpeg';
+import elephant from './assets/images/elephant.jpeg';
+import fish from './assets/images/fish.jpeg';
+import fox from './assets/images/fox.jpeg';
+import lizard from './assets/images/lizard.jpeg';
+import rabbit from './assets/images/rabbit.jpeg';
+import racoon from './assets/images/racoon.jpeg';
+import turtle from './assets/images/turtle.jpeg';
+import animalData from './components/animalData';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Pet Game</h1>
+        <img src={feline} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ pet-game/src/components/animalData.js
@@ -0,0 +1,75 @@
+import feline from '../App';
+import dino from '../App';
+import canine from '../App';
+import elephant from '../App';
+import fish from '../App';
+import fox from '../App';
+import lizard from '../App';
+import rabbit from '../App';
+import racoon from '../App';
+import turtle from '../App';
+
+const animalData = [
+  {
+    id: 1,
+    name: feline,
+    image: feline,
+    rarity: 1
+  },
+  {
+    id: 2,
+    name: canine,
+    image: canine,
+    rarity: 1
+  },
+  {
+    id: 3,
+    name: fish,
+    image: fish,
+    rarity: 1
+  },
+  {
+    id: 4,
+    name: lizard,
+    image: lizard,
+    rarity: 1
+  },
+  {
+    id: 5,
+    name: rabbit,
+    image: rabbit,
+    rarity: 2
+  },
+  {
+    id: 6,
+    name: turtle,
+    image: turtle,
+    rarity: 2
+  },
+  {
+    id: 7,
+    name: fox,
+    image: fox,
+    rarity: 2
+  },
+  {
+    id: 8,
+    name: racoon,
+    image: racoon,
+    rarity: 3
+  },
+
+  {
+    id: 9,
+    name: dino,
+    image: dino,
+    rarity: 3
+  },
+  {
+    id: 10,
+    name: elephant,
+    image: elephant,
+    rarity: 3
+  }
+];
+export default animalData;
