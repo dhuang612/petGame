3b1acd464b35b74a1489c86b957f7958b00be8f9
dhuang612 <dhuang@madisoncollege.edu>
2019-07-29 15:22:25 -0400

    created function for rolling a random number and saving it into state

--- pet-game/src/components/MainPetPage.js
+++ pet-game/src/components/MainPetPage.js
@@ -3,42 +3,80 @@ import React, { Component } from 'react';
 class MainPetPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      petTier: []
+    };
   }
+  componentDidUpdate = () => {};
+
   //accounting for rarity 3 is the hardest to get and 1 is the easiest.
-  RollPetRarity = () => {
+  rollPetRarity = () => {
     //you can reroll for a pet twice
+    const rollResult = [];
 
-    const randomNum = Math.floor(Math.random() * 4) - 0.65;
-
-    if (randomNum < 0 || randomNum < 1) {
-      return 1;
-    } else if (randomNum > 1 && randomNum < 2) {
-      return 2;
-    } else {
-      return 3;
+    while (rollResult.length === 0) {
+      const randomNum = Math.floor(Math.random() * 4) - 0.65;
+      if (randomNum < 0 || randomNum < 1) {
+        rollResult.push(1);
+      } else if (randomNum > 1 && randomNum < 2) {
+        rollResult.push(2);
+      } else {
+        rollResult.push(3);
+      }
+      return rollResult;
     }
+
+    this.setState({ petTier: rollResult }, () =>
+      console.log(this.state.petTier)
+    );
   };
-  GetPet = num => {
-    console.log(this.RollPetRarity());
 
-    if (num === 1) {
+  getPet = petTier => {
+    if (petTier === 1) {
       let firstTier = ['cat', 'dog', 'fish', 'lizard'];
+      return 1;
     }
-    if (num === 2) {
+    if (petTier === 2) {
       let secondTier = ['rabbit', 'turtle', 'fox'];
+      return 2;
     }
-    if (num === 3) {
+    if (petTier === 3) {
       let thirdTier = ['racoon', 'dino', 'elephant'];
+      return 3;
     }
   };
 
   render() {
+    const { petTier } = this.state;
     return (
       <div>
         <h1> Main page</h1>
-        {this.GetPet()}
+        {console.log(petTier)}
+        {this.rollPetRarity()}
       </div>
     );
   }
 }
 export default MainPetPage;
+
+/*
+well, in general, you can't reliably check the 
+state except in render or if you use the function callback version
+
+
+trying to call this.setState is causing the following error:
+this.setState({
+      petTier: results.roll
+    });
+
+    Maximum update depth exceeded. This can happen when a
+     component repeatedly calls setState inside 
+    componentWillUpdate or componentDidUpdate. 
+    React limits the number of nested updates to prevent infinite loops.
+
+
+
+      const results = rollResult.map(item => ({
+      results: item
+    }));
+*/
