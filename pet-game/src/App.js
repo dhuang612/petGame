3b1c5c1216547d87b1b06f27d8414f430dcf70e4
dhuang612 <dhuang@madisoncollege.edu>
2019-07-29 18:06:06 -0400

    starting on getting images to be added for pet

--- pet-game/src/App.js
+++ pet-game/src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import MainPage from './components/MainPetPage';
 import './App.css';
-import feline from './assets/images/cat.jpeg';
-import dino from './assets/images/dinosaur.jpeg';
-import canine from './assets/images/dog.jpeg';
-import elephant from './assets/images/elephant.jpeg';
-import fish from './assets/images/fish.jpeg';
-import fox from './assets/images/fox.jpeg';
-import lizard from './assets/images/lizard.jpeg';
-import rabbit from './assets/images/rabbit.jpeg';
-import racoon from './assets/images/racoon.jpeg';
-import turtle from './assets/images/turtle.jpeg';
-import animalData from './components/animalData';
 
 class App extends Component {
   constructor(props) {
--- pet-game/src/components/MainPetPage.js
+++ pet-game/src/components/MainPetPage.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
+import cat from '../assets/images/cat.jpeg';
+import dino from '../assets/images/dinosaur.jpeg';
+import dog from '../assets/images/dog.jpeg';
+import elephant from '../assets/images/elephant.jpeg';
+import fish from '../assets/images/fish.jpeg';
+import fox from '../assets/images/fox.jpeg';
+import lizard from '../assets/images/lizard.jpeg';
+import rabbit from '../assets/images/rabbit.jpeg';
+import racoon from '../assets/images/racoon.jpeg';
+import turtle from '../assets/images/turtle.jpeg';
+import animalData from '../components/animalData';
+
 class MainPetPage extends Component {
   constructor(props) {
     super(props);
@@ -14,55 +26,53 @@ class MainPetPage extends Component {
   //accounting for rarity 3 is the hardest to get and 1 is the easiest.
   rollPetRarity = () => {
     //you can reroll for a pet twice
-    const rollResult = [];
+    let rollResult = '';
 
     const randomNum = Math.floor(Math.random() * 4) - 0.65;
     if (randomNum < 0 || randomNum < 1) {
-      rollResult.push(1);
+      return (rollResult = 1);
     } else if (randomNum > 1 && randomNum < 2) {
-      rollResult.push(2);
+      return (rollResult = 2);
     } else {
-      rollResult.push(3);
+      return (rollResult = 3);
     }
 
     return rollResult;
   };
 
   randomPetSelection = tiers => {
-    let selectedPet = [];
     let random = Math.random();
     let pets = [];
     const rolledNum = this.rollPetRarity();
     console.log(rolledNum);
-    if (rolledNum == 1) {
+    if (rolledNum === 1) {
       pets = [...this.state.firstTier];
       let totalPets = pets.length;
       let randomIndex = Math.floor(random * totalPets);
       let randomPet = pets[randomIndex];
       return randomPet;
     }
-    if (rolledNum == 2) {
+    if (rolledNum === 2) {
       pets = [...this.state.secondTier];
       let totalPets = pets.length;
       let randomIndex = Math.floor(random * totalPets);
       let randomPet = pets[randomIndex];
       return randomPet;
-    } else if (rolledNum == 3) {
+    } else if (rolledNum === 3) {
       pets = [...this.state.thirdTier];
       let totalPets = pets.length;
       let randomIndex = Math.floor(random * totalPets);
       let randomPet = pets[randomIndex];
       return randomPet;
     }
-    console.log();
   };
 
   render() {
     return (
       <div>
         <h1> Main page</h1>
-
         {this.randomPetSelection()}
+        <img src={this.randomPetSelection} />
       </div>
     );
   }
